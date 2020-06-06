f0b57144151452a31a81beb730fa2df4870dd909
Valtteri Pyyhtiä <valtteri@valtteri.io>
2020-06-06 23:56:17 +0300

    Add variables to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,8 @@
 ((nil .
       ((eval . (setq custom-application-directory "~/Projects/OpenGL-app/build/emacs"))
        (eval . (setq custom-application-runnable "./OpenGL-app"))
+       (eval . (setq custom-application-test-directory "~/Projects/OpenGL-app/build/emacs/tests"))
+       (eval . (setq custom-application-test-runnable "./RunTests"))
        (cmake-ide-project-dir . "~/Projects/OpenGL-app")
        (cmake-ide-build-dir . "~/Projects/OpenGL-app/build/emacs")
        (cmake-ide-cmake-opts . "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++")
