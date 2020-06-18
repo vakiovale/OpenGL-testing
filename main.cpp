6479937eca750377d62de5d2e329dac401180298
Valtteri Pyyhtiä <valtteri@valtteri.io>
2020-06-18 20:10:16 +0300

    Wrap main app inside try-catch

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <exception>
 #include "src/app.h"
 
 void runApplication() {
-    App app;
-    app.run();
+    try {
+        App app;
+        app.run();
+    } catch (const std::exception& exception) {
+        std::cout << exception.what() << std::endl;
+    }
 }
 
 int main() { runApplication(); }
